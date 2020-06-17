7a7c2a3b6d0b3b90b0ddc71161aa4ada58fbd916
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-06-16 17:32:50 -0700

    Added basic structure of website.

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import MyNavbar from './MyNavbar';
+import MyFooter from './MyFooter';
+import ReactPlayer from 'react-player';
+
+const Home = () => {
+  return (
+    <div>
+      <MyNavbar />
+      <div className='p-3'>
+          Hi! My name is Jan Charatan and I'm a rising sophomore studying computer science at Pomona College in Claremont, California. 
+          Although I was born in Portland, Oregon, I've also lived in Dresden, Germany and San Jose, California. I'm interested in 
+          frontend development using React (what I used to make this webpage!). Some of my other interests include soccer, poker, skiing, 
+          videography and vexillology. I also have a YouTube channel called Pigunas; here's one of my videos:
+      </div>
+      <div className='d-flex justify-content-center'>
+        <ReactPlayer className='ml-4' url='https://www.youtube.com/watch?v=2POFdJANTjs' />
+      </div>
+      <MyFooter />
+    </div>
+    
+  );
+}
+
+export default Home;
---
+++ src/MyFooter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyFooter = () => (
+  <div className='fixed-bottom d-flex justify-content-center mb-3'>
+    jan.charatan@gmail.com | (503) 419-8778
+  </div>
+)
+
+export default MyFooter;
