cdc62b302dd61b4a13bb47e4ac198fe8b359484d
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-06-26 18:44:53 -0700

    Updates

---
+++ precache-manifest.a8a60d63b623b3e34c1f8288850fa188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5fa029fad0acecc3c93750194449da",
+    "url": "/jancharatan/index.html"
+  },
+  {
+    "revision": "74b1e7bc841228aa1a08",
+    "url": "/jancharatan/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "74b1e7bc841228aa1a08",
+    "url": "/jancharatan/static/js/2.0111eeaf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/jancharatan/static/js/2.0111eeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b31cc183b3b5f50c07",
+    "url": "/jancharatan/static/js/main.2d5fcfd1.chunk.js"
+  },
+  {
+    "revision": "f8243da58ba862df33fd",
+    "url": "/jancharatan/static/js/runtime-main.ba0298f1.js"
+  }
+]);
